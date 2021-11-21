88d532e755cce2c487df92abe25840f3da3358d2
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2021-11-22 01:48:19 +0530

    removed redux devtools on prod mode

--- src/store/index.js
+++ src/store/index.js
@@ -7,10 +7,14 @@ const sagaMiddleware = createSagaMiddleware()
 
 const middleware = [sagaMiddleware]
 
-const store = compose(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)(createStore)(reducers)
+//While on Production Mode
+const store = compose(applyMiddleware(...middleware))(createStore)(reducers)
+
+//// While Debugging (Development Mode)
+// const store = compose(
+//   applyMiddleware(...middleware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )(createStore)(reducers)
 
 sagaMiddleware.run(rootSaga)
 
