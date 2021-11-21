30f99d8baf9479807e3090c88ed8f61407f3e6e8
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2021-11-22 01:03:29 +0530

    implemented ui filtering

--- src/components/UsersList/UsersList.component.jsx
+++ src/components/UsersList/UsersList.component.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { loadUsers } from "../../actions/index"
 import { useSelector, useDispatch } from "react-redux"
 import UserCard from "../UserCard/UserCard.component"
@@ -7,22 +7,41 @@ import "./UserList.styles.scss"
 
 function UsersList() {
   const dispatch = useDispatch()
-  const users = useSelector((state) => state.users.users)
+  const usersList = useSelector((state) => state.users.users)
   const loading = useSelector((state) => state.users.loading)
   const error = useSelector((state) => state.users.error)
+  const [users, setUsers] = useState(usersList)
+  const [searchTerm, setSearchTerm] = useState("")
 
   useEffect(() => {
     dispatch(loadUsers())
-  }, [])
+  }, [dispatch])
+
+  //UI filtering
+  useEffect(() => {
+    const filteredUsers = usersList?.filter((user) =>
+      user.name.first.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    setUsers(filteredUsers)
+  }, [searchTerm, usersList])
 
   if (loading) {
     return <div>{loading && <Spinner />}</div>
   }
+
   return (
-    <div className="user-list-container">
-      {error && !loading && <h2>{error}</h2>}
-      {users && users.map((user, i) => <UserCard key={i} {...user} />)}
-    </div>
+    <>
+      <div className="search-user">
+        <input
+          placeholder="Search user..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="user-list-container">
+        {error && !loading && <h2>{error}</h2>}
+        {users && users.map((user, i) => <UserCard key={i} {...user} />)}
+      </div>
+    </>
   )
 }
 
